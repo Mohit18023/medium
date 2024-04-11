e14e8ee8a8a5d9e8b028c210905abbd230350ff3
mohit choudhary <mohitchoudhary1054@gmail.com>
2024-04-11 05:55:17 +0530

    components/Avatar.tsx

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,6 +1,6 @@
 const Avatar = ({ name }: { name: string }) => {
   return (
-    <div className="relative inline-flex items-center justify-center w-4 h-4 overflow-hidden bg-gray-600 rounded-full ">
+    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-600 rounded-full ">
       <span className="text-xs font-extralight text-gray-100 ">
         {name[0]}
       </span>
